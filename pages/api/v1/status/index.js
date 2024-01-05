b80dcbc250328d5f8ab9593f6a347f2c1e4012f2
ogomesalef <alefgomesandre@gmail.com>
2024-01-05 17:18:56 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
+  const updatedAt = new Date().toISOString();
 
-  console.log(result.rows[0]);
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  response.status(200).json(result);
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
-test("GET on /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const data = await response.json();
+  const date = new Date(data.updated_at);
+
   expect(response.status).toBe(200);
+
+  expect(date.toISOString()).toBe(data.updated_at);
+
+  expect(data).toHaveProperty("updated_at");
+  expect(data.dependencies.database).toHaveProperty("max_connections");
+  expect(data.dependencies.database).toHaveProperty("opened_connections");
+  expect(data.dependencies.database).toHaveProperty("version");
+
+  expect(data.dependencies.database.version).toBe("16.0");
+  expect(data.dependencies.database.max_connections).toBe(100);
+  expect(data.dependencies.database.opened_connections).toBe(1);
+
+  expect(typeof data.dependencies.database.max_connections).toBe("number");
+  expect(typeof data.dependencies.database.opened_connections).toBe("number");
+
+  // console.log(typeof data.dependencies.database.max_connections)
 });
